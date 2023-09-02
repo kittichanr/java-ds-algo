d225443cdb723e25f184bb0da8bf892f4ff464e0
kittichanr <petchkubbb@gmail.com>
2023-09-02 09:13:20 +0700

    stack

---
+++ src/main/java/stack/Main.java
@@ -0,0 +1,45 @@
+package stack;
+
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        // *******************************************************
+
+        // stack = LIFO data structure. Last-In First-Out
+        //               stores objects into a sort of "vertical tower"
+        //               push() to add objects to the top
+        //               pop() to remove objects from the top
+
+        // uses of stacks?
+        // 1. undo/redo features in text editors
+        // 2. moving back/forward through browser history
+        // 3. backtracking algorithms (maze, file directories)
+        // 4. calling functions (call stack)
+
+        // *******************************************************
+
+        Stack<String> stack = new Stack<String>();
+
+        System.out.println(stack.empty());
+
+        stack.push("Minecraft");
+        stack.push("Skyrim");
+        stack.push("Doom");
+        stack.push("Boarderlands");
+        stack.push("FFVII");
+
+        String myFavGame = stack.pop();
+
+        System.out.println(stack);
+        System.out.println(myFavGame);
+        System.out.println(stack.peek());
+        System.out.println(stack.search("Minecraft"));
+        System.out.println(stack.search("Test"));
+
+        // Java heap space
+        for (int i = 0; i < 1000000000; i++) {
+            stack.push("Skyrim");
+        }
+    }
+}
