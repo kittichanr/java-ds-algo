0eace8de27be6fee575dccdd4281397559398036
kittichanr <petchkubbb@gmail.com>
2023-09-10 07:06:15 +0700

    Calculate execution time

---
+++ src/main/java/cal_execution_time/Main.java
@@ -0,0 +1,16 @@
+package cal_execution_time;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        long start = System.nanoTime();
+
+        // --------------  program ------------------
+
+        Thread.sleep(3000);
+
+        // ------------------------------------------
+
+        long duration = (System.nanoTime() - start) / 1000000;
+        System.out.println(duration + "ms");
+    }
+}
